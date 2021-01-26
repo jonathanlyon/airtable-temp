f3696a22a47881c660f37fa85b3f0a5c776f6d67
Jonathan Lyon <jonathan.lyon@gmail.com>
2021-01-27 05:08:53 +1300

    added getOnlinemembers.js

--- functions/members.js
+++ functions/members.js
@@ -1,5 +1,6 @@
 const formattedReturn = require('./helpers/formattedReturn');
 const getCourses = require('./helpers/getCourses');
+const getOnlinemembers = require('./helpers/getOnlinemembers');
 const createCourse = require('./helpers/createCourse');
 const deleteCourse = require('./helpers/deleteCourse');
 const updateCourse = require('./helpers/updateCourse');
@@ -7,7 +8,7 @@ const getRecords = require('./helpers/getRecords');
 exports.handler = async (event) => {
     // TODO: call appropriate helper function based on HTTP method
      if(event.httpMethod = 'Get') {
-         return await getCourses(event);
+         return await getOnlinemembers(event);
      } else {
          return formattedReturn(405, {});
      }
