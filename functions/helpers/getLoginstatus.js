0547603bdda8cb223d4178c903e850b374ed285c
Jonathan Lyon <jonathan.lyon@gmail.com>
2021-01-29 11:34:46 +1300

    changed function of log in status to return count of logged in users

--- functions/helpers/getLoginstatus.js
+++ functions/helpers/getLoginstatus.js
@@ -1,5 +1,18 @@
 const { table } = require('./airtable');
 const formattedReturn = require('./formattedReturn');
+
+function toBoolean( o ) {
+    if ( null !== o ) {
+        let t = typeof o;
+        if ( "undefined" !== typeof o ) {
+            if ( "string" !== t ) return !!o;
+            o = o.toLowerCase().trim();
+            return "true" === o || "1" === o;
+        }
+    }
+    return false;
+}
+
 module.exports = async (event) => {
     // TODO: update course
     const recordId = event.queryStringParameters.recordid;
@@ -8,14 +21,23 @@ module.exports = async (event) => {
     console.log(recordId);
     console.log(online);
 
-    const updateCode = ({ id: `${recordId}`,fields:{"online":`${online}`}});
+    console.log('Convert param to bool',toBoolean(online));
+
+    const updateCode = ({ id: `${recordId}`,fields:{"connected":toBoolean(online)}});
     
     const updateArray = [];
     updateArray.push(updateCode);
     console.log(updateArray);
 
     const getWebhook = await table.update(updateArray);
+
+    const members = await table.select({
+        view: 'LoggedIn'
+      })
+      .all();
+
+    const onlinememberCount = members.length;    
     //return formattedReturn(200, {msg:'updated ' + recordId + ' with ' + online});
-    return formattedReturn(200, updateArray);
+    return formattedReturn(200, onlinememberCount);
 
 };
