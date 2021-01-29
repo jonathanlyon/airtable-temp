f75d514b0ee768c4928dcbf75f2ce72b231d0da7
Jonathan Lyon <jonathan.lyon@gmail.com>
2021-01-30 08:35:36 +1300

    added try catch and assigned variable toBoolean for login status

--- functions/helpers/getLoginstatus.js
+++ functions/helpers/getLoginstatus.js
@@ -14,16 +14,19 @@ function toBoolean( o ) {
 }
 
 module.exports = async (event) => {
+    try{
     // TODO: update course
     const recordId = event.queryStringParameters.recordid;
     const online = event.queryStringParameters.online;
+    const status = toBoolean(online);
 
     console.log(recordId);
     console.log(online);
 
-    console.log('Convert param to bool',toBoolean(online));
+    console.log('Convert param to bool',status);
 
-    const updateCode = ({ id: `${recordId}`,fields:{"connected":toBoolean(online)}});
+    const updateCode = ({ id: `${recordId}`,fields:{"connected":status}});
+
     
     const updateArray = [];
     updateArray.push(updateCode);
@@ -39,5 +42,10 @@ module.exports = async (event) => {
     const onlinememberCount = members.length;    
     //return formattedReturn(200, {msg:'updated ' + recordId + ' with ' + online});
     return formattedReturn(200, onlinememberCount);
+    }
+    catch(err) {
+        console.error(err);
+        return formattedReturn(500, { msg: 'err' });
+    }
 
 };
