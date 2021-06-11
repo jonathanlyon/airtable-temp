e330a9eb0c9cce47c14c70c9f70bfdf5c42c0a65
Jonathan Lyon <jonathan.lyon@gmail.com>
2021-06-12 07:16:36 +1200

    added IP address

--- functions/onlinemembercount.js
+++ functions/onlinemembercount.js
@@ -1,12 +1,10 @@
 const formattedReturn = require('./helpers/formattedReturn');
 const getOnlinecount = require('./helpers/getOnlinecount');
 
-exports.handler = async (event) => {
-    // TODO: call appropriate helper function based on HTTP method
-     if(event.httpMethod = 'Get') {
-         return await getOnlinecount(event);
-     } else {
-         return formattedReturn(405, {});
-     }
- 
-};
+exports.handler = function(event, context, callback) {
+    console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
+    callback(null, {
+    statusCode: 200,
+    body: `Your IP address ${event.headers['client-ip']}`
+    });
+  }
